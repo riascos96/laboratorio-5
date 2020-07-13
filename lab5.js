8af01d4f4f4a6d7a23c52ebc593f754703d1d5ee
Vagrant Default User <vagrant@buster.localdomain>
2020-07-13 03:14:29 +0000

    laboratorio_5

---
+++ lab5.js
@@ -0,0 +1,52 @@
+
+
+
+var  num1 = 6 ;
+var  num2 = 2 ;
+
+function  sumar()  {
+    console.log (  ( num1 + num2 ) + "sumar con var" );
+}
+
+function  restar()  {
+    console.log (  ( num1 - num2 ) +"restar con var" ) ;
+}
+
+function  multiplicar ()  {
+    console.log (  ( num1 * num2 ) + "multiplicar con var"  ) ;
+}
+
+function  dividir()  {
+    console.log (  ( num1 / num2 ) + "dividir con var"  ) ;
+}
+
+sumar() ;
+restar() ;
+multiplicar() ;
+dividir() ;
+
+
+console.log ( "--------------------------  2   ------------------------------" )
+let  a = 5 ;
+let  b = 6 ;
+
+console.log ( "a" + b ) ;
+
+
+console.log ( "--------------------------  3   ------------------------------" )
+const  c = 34 ;
+const  d = "hola" ;
+
+console.log ( typeof ( c ) ) ;
+console.log ( typeof ( d ) ) ;
+
+
+console.log ( "--------------------------  4   ------------------------------" )
+
+let  ent = 8 ;
+let  str = "laboratorio 5" ;
+let  bool = true ;
+
+const  obj = { ent , str , bool ,  } ;
+
+console.log ( obj ) ;
